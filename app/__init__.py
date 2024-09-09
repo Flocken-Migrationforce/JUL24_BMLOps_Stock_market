f5d5c9683ce03ecfa2bf147861928ea82981f057
FLOCKENl <itflocken@web.de>
2024-09-09 12:43:13 +0200

    Extensive file clean-up and archivation

--- app/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file exists to ensure the 'app' folder is considered as a package by Python. Even if this file is empty, it is required.
--- others/auth.py
+++
@@ -1,2 +0,0 @@
-def authenticate_user(username: str, password: str, users_db: dict) -> bool:
-    return username in users_db and users_db[username] == password
